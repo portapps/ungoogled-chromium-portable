3df29aa8563f74886107a12aa8537270248dcc4f
CrazyMax <1951866+crazy-max@users.noreply.github.com>
2026-04-04 02:47:28 +0200

    Portapps 3.18.0

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 //go:generate go install -v github.com/josephspurrier/goversioninfo/cmd/goversioninfo
-//go:generate goversioninfo -icon=res/papp.ico -manifest=res/papp.manifest
 package main
 
 import (
 	"os"
 	"path"
+	"path/filepath"
 
 	"github.com/portapps/portapps/v3"
 	"github.com/portapps/portapps/v3/pkg/log"
@@ -36,7 +36,7 @@ func init() {
 
 func main() {
 	utl.CreateFolder(app.DataPath)
-	app.Process = utl.PathJoin(app.AppPath, "chrome.exe")
+	app.Process = filepath.Join(app.AppPath, "chrome.exe")
 	app.Args = []string{
 		"--user-data-dir=" + app.DataPath,
 		"--no-default-browser-check",
